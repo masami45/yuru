1766513346097222bee25131f8bc9f5509b91a3c
Miku <masami45@tuta.io>
2021-01-13 18:10:15 +0700

    adding and change the function of the file

---
+++ bin/compile.js
@@ -0,0 +1,34 @@
+(async function() {
+  try {
+    const exec = require("./Utils").exec;
+
+    // Remove old dist folder
+    console.log("[CLEANUP] Removing old dist folder...");
+    await exec("rm -rf ./dist");
+    console.log("[CLEANUP] Finished removing the old dist folder.");
+  
+    // Start to compile the project
+    console.log("[COMPILE] Compile the project using the TSC compiler...");
+    let startTime = new Date(); // Start the timer
+    await exec("tsc");
+    let endTime = new Date(); // End the timer
+    console.log(`[COMPILE] Finished compiling the project, time to compile: ${msToTime(startTime, endTime)}.`);
+  } catch (error) { // Catch if there is an error from command
+    console.error(error);
+  }
+})();
+
+/**
+ * Convert ms to readable timer
+ *
+ * @param {number} start The start time
+ * @param {number} end The end time
+ * @return {string} Readable timer hh:mm:ss.ms
+ */
+function msToTime(start, end) {
+  let diff = end.getTime() - start.getTime();
+  let pad  = (n) => (n < 10) ? `0${n}` : n;
+  //            hours               minutes                    seconds               ms
+  return `${pad(diff/3.6e6|0)}:${pad((diff%3.6e6)/6e4|0)}:${pad((diff%6e4)/1e3|0)}.${diff%1e3}`;
+}
+
