043428a7f4fcd2808148a9c68e0a15314504b5e0
Miku <masami45@tuta.io>
2021-01-13 18:11:27 +0700

    fixing typo

--- bin/lint.js
+++ bin/lint.js
@@ -5,9 +5,10 @@
     const argv = process.argv.slice(2);
     const command = "eslint --ignore-path .gitignore 'src/**/*'";
     
-    // Start the linting
+    // Start the linter
     console.log("[LINT] Starting lint your code...");
-    // fix the command if there is fix options
+
+    // If using "fix" args, run this condition first
     if (argv[0] == "fix") {
       console.log("[LINT] Fixing your code...");
       await exec(`${command} --fix`);
@@ -16,10 +17,10 @@
     }
 
     await exec(command); 
-    // Print this IF there is no error when linting
+    // If there is no error when linting, print this message
     console.log("[LINT] No error has found on your code, good work!");
-  } catch (error) { // catch error from command
-    // IF there is an error when linting, print this instead above one
+  } catch (error) { // Catch error from command
+    // Message that get printed if there is an error when linting the code
     if (error.code == 1) {
       console.log("[LINT] Error has occurred on your code! The list of errors is above.");
       console.log("[LINT] To fix error, use `lint:fix` command.")
